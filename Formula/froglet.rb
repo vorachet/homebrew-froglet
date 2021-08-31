2706f3b24eb8538e4cb0907af32c0d0695f5689d
vorachet <vorachet@gmail.com>
2021-08-31 19:14:37 +0700

    add

---
+++ Formula/froglet.rb
@@ -0,0 +1,11 @@
+class Froglet < Formula
+    homepage "https://github.com/vorachet/froglet"
+    url "https://github.com/vorachet/froglet/archive/refs/tags/0.1.tar.gz"
+    version "0.1"
+    sha256 "f765382b0cf05f0ccc0b2a4c7b3785d344a3442fe005626953c457bd69a25ba8"
+  
+    def install
+        bin.install "./froglet"
+    end
+
+  endFormula
