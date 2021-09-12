88125de1a79b8d23f60d7858861d5b7e9fa49fbc
vorachet <vorachet@gmail.com>
2021-09-12 21:42:32 +0700

    release 0.2

--- Formula/froglet.rb
+++ Formula/froglet.rb
@@ -1,8 +1,8 @@
 class Froglet < Formula
     homepage "https://github.com/vorachet/froglet"
-    url "https://github.com/vorachet/froglet/archive/refs/tags/0.1.tar.gz"
+    url "https://github.com/vorachet/froglet/archive/refs/tags/0.2.tar.gz"
     version "0.1"
-    sha256 "f765382b0cf05f0ccc0b2a4c7b3785d344a3442fe005626953c457bd69a25ba8"
+    sha256 "39e961428be92d5b664541f3a4419409c16b3aa15a0ba4a795dbf730d8fc66be"
   
     def install
         bin.install "./froglet"
